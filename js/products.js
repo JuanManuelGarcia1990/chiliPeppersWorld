01ba3548a0b8a0bea903e49249350b24f5ea0aa1
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2022-12-08 11:54:44 -0300

    tienda

---
+++ js/products.js
@@ -0,0 +1,97 @@
+const productos = [
+  {
+  "nombre": "Remera 1",
+  "precio":  5500,
+  "cantidad": 12,
+  "id": 1,
+  "img": "../assets/tienda/remera1.jpg"
+  },
+
+  {
+  "nombre": "Remera 2",
+  "precio":  5700,
+  "cantidad": 10,
+  "id": 2,
+  "img": "../assets/tienda/remera2.jpg"
+  },
+
+  {
+  "nombre": "Remera 3",
+  "precio": 5800,
+  "id": 3,
+  "cantidad": 11,
+  "img": "../assets/tienda/remera3.jpg"
+  },
+
+  {
+  "nombre": "Remera 4",
+  "precio": 5650,
+  "id": 4,
+  "cantidad": 7,
+  "img": "../assets/tienda/remera4.jpg"
+  },
+
+  {
+  "nombre": "Remera 5",
+  "precio": 5770,
+  "id": 5,
+  "cantidad": 3,
+  "img": "../assets/tienda/remera5.jpg"
+  },
+
+  {
+  "nombre": "Remera 6",
+  "precio": 5200,
+  "id": 6,
+  "cantidad": 7,
+  "img": "../assets/tienda/remera6.jpg"
+  },
+
+  {
+  "nombre": "Remera 7",
+  "precio": 5550,
+  "id": 7,
+  "cantidad": 9,
+  "img": "../assets/tienda/remera7.jpg"
+  },
+
+  {
+  "nombre": "Musculosa",
+  "precio":  5500,
+  "id": 8,
+  "cantidad": 4,
+  "img": "../assets/tienda/musculosa8.jpg"
+  },
+
+  {
+  "nombre": "Remera 8",
+  "precio": 6000,
+  "id": 9,
+  "cantidad": 8,
+  "img": "../assets/tienda/remera9.jpg"
+  },
+
+  {
+  "nombre": "Gorra 1",
+  "precio": 4500,
+  "id": 10,
+  "cantidad": 15,
+  "img": "../assets/tienda/gorra10.jpg"
+  },
+
+  {
+  "nombre": "Gorra 2",
+  "precio": 4500,
+  "id": 11,
+  "cantidad": 16,
+  "img": "../assets/tienda/gorra11.jpg"
+  },
+
+  {
+  "nombre": "Taza",
+  "precio": 4500,
+  "id": 12,
+  "cantidad": 18,
+  "img": "../assets/tienda/taza12.jpg"
+  }
+]
