87086acc0d1530db21f1b85f0ada605006c16361
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2022-12-15 10:43:02 -0300

    cambios

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,5 @@
 let compra = new Compra();
-
+//Crear Modal Carrito
 const modalCarrito = () => {
   modalContainer.innerHTML = "";
   modalContainer.style.display = "flex";
@@ -9,17 +9,16 @@ const modalCarrito = () => {
         <h1 class="modal-header-title">Carrito</h1>
       `;
   modalContainer.append(modalHeader);
-
+  //Boton de cierre del modal
   const modalbutton = document.createElement("h1");
   modalbutton.innerText = "x";
   modalbutton.className = "modal-header-button";
-
   modalbutton.addEventListener("click", () => {
     modalContainer.style.display = "none";
   });
 
   modalHeader.append(modalbutton);
-
+  //Mostrar Productos seleccionados
   carrito.forEach((product) => {
     let carritoContent = document.createElement("div");
     carritoContent.className = "modal-content";
@@ -35,9 +34,8 @@ const modalCarrito = () => {
         `;
 
     modalContainer.append(carritoContent);
-
+    //Restar productos en el modal
     let restar = carritoContent.querySelector(".restar");
-
     restar.addEventListener("click", () => {
       if (product.cantidad !== 1) {
         product.cantidad--;
@@ -45,64 +43,53 @@ const modalCarrito = () => {
       saveLocal();
       modalCarrito();
     });
-
+    //Sumar productos en el modal
     let sumar = carritoContent.querySelector(".sumar");
     sumar.addEventListener("click", () => {
       product.cantidad++;
       saveLocal();
       modalCarrito();
     });
-
+    //Eliminar productos del modal
     let eliminar = carritoContent.querySelector(".delete-product");
-
     eliminar.addEventListener("click", () => {
       eliminarProducto(product.id);
     });
   });
-
+  //Precio Total
   // const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
-
   const totalBuying = document.createElement("div");
   totalBuying.className = "total-content";
-  totalBuying.innerHTML = `<p id="total">$ ${precioTotal}</p> `;
+  totalBuying.innerHTML = `<p id="total">$ ${compra.totalAPagar}</p> `;
   modalContainer.append(totalBuying);
 
-
   // Boton Comprar
-  const modalCompra = document.createElement("div")
+  const modalCompra = document.createElement("div");
   modalCompra.className = "modal-compra";
-  modalContainer.append(modalCompra)
-  const botonCompra = document.createElement("button")
+  modalContainer.append(modalCompra);
+  const botonCompra = document.createElement("button");
   botonCompra.innerText = "Comprar";
   botonCompra.className = "botonCompra";
-  modalCompra.append(botonCompra)
-
+  modalCompra.append(botonCompra);
 };
-
-
+//Ver productos dentro del modal
 verCarrito.addEventListener("click", modalCarrito);
-
+//Eliminar Productos dentro del modal
 const eliminarProducto = (id) => {
   const foundId = carrito.find((element) => element.id === id);
-
   console.log(foundId);
-
   carrito = carrito.filter((carritoId) => {
     return carritoId !== foundId;
   });
-
   carritoCounter();
   saveLocal();
   modalCarrito();
 };
-
+//Muestra cantidad de elementos agregados al carrito
 const carritoCounter = () => {
   cantidadCarrito.style.display = "block";
-
   const carritoLength = carrito.length;
-
   localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
-
   cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
 };
 
--- js/classCompra.js
+++ js/classCompra.js
@@ -5,9 +5,9 @@ class Compra {
   totalAPagar() {
     return carrito.reduce((acc, el) =>  acc + el.precio * el.cantidad, 0).toFixed(2);
   }
-  totalCompra() {
-    let precioTotal = document.getElementById("#total");
-    precioTotal.innerHTML = `${this.totalAPagar()}`
-  }
+  // totalCompra() {
+  //   let precioTotal = document.getElementById("#total");
+  //   precioTotal.innerHTML = `${this.totalAPagar()}`
+  // }
 }
   
